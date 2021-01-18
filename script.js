0c5b62e1a18e793fd88b1576474a1b51d197bbc1
Saba <40300784+sabaeky@users.noreply.github.com>
2021-01-18 23:15:59 +0330

    including icons for each domain

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ window.addEventListener("load" ,()=>{
         lat=position.coords.latitude;
         const proxy="https://cors-anywhere.herokuapp.com/";
     
-            const api=`${proxy}api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=02060e118da723a8af10b3e7a7f0042c    `
+            const api=`${proxy}api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=dab3af44de7d24ae7ff86549334e45bd     `
     
             fetch(api).then((response)=>{
     
@@ -31,6 +31,7 @@ window.addEventListener("load" ,()=>{
     
     
             })
+    
             .then (data =>
                 {
     
@@ -42,10 +43,49 @@ window.addEventListener("load" ,()=>{
                         loc.textContent=name;
                         climate.textContent=main;
                         tempvalue.textContent=Math.round(feels_like-273);
+                        if(id<300 && id>200)
+                        {
+                            tempicon.src="./icons/thunderstorm.svg"
+                        }
+                       else  if(id<400 && id>300)
+                        {
+                            tempicon.src="./icons/cloud-solid.svg"
+                        }
+                       else if(id<600&& id>500)
+                        {
+                            tempicon.src="./icons/rain.svg"
+                        }
+                       else  if(id<700 && id>600)
+                        {
+                            tempicon.src="./icons/snow.svg"
+                        }
+                       else  if(id<800 && id>700)
+                        {
+                            tempicon.src="./icons/clouds.svg"
+                        }
+                         else if(id==800)
+                        {
+                            tempicon.src="./icons/clouds-and-sun.svg"
+                        }
+    
+    
+    
+    
+    
                         console.log(data);
-
-
-                    })
+    
+    
+                })
+    
+    
+    
+    }
         
+        
+        
+        )}
+    
+    
+    })
         
      
