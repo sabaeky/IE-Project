30c5df7926cbd8e6cc6eae09336898f1474290c2
Saba <40300784+sabaeky@users.noreply.github.com>
2021-01-18 22:36:40 +0330

    variables added

---
+++ script.js
@@ -0,0 +1,7 @@
+let loc =document.getElementById("location");
+let tempicon=document.getElementById("temp-icon");
+let tempvalue=document.getElementById("temp-value");
+let climate =document.getElementById("climate");
+let iconfile;
+const searchInput=document.getElementById("search-input");
+const searchButton=document.getElementById("search-button");
