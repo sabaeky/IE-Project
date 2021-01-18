0200ffe61cb77d0a40b0f1d21140d6141ee3b68d
Saba <40300784+sabaeky@users.noreply.github.com>
2021-01-18 23:03:47 +0330

    fetch api and use cors

--- script.js
+++ script.js
@@ -5,3 +5,47 @@ let climate =document.getElementById("climate");
 let iconfile;
 const searchInput=document.getElementById("search-input");
 const searchButton=document.getElementById("search-button");
+
+window.addEventListener("load" ,()=>{
+
+    let long;
+    let lat;
+    
+    if(navigator.geolocation)
+    {
+    
+        navigator.geolocation.getCurrentPosition((position)=>
+        {
+    
+       
+        
+        long=position.coords.longitude;
+        lat=position.coords.latitude;
+        const proxy="https://cors-anywhere.herokuapp.com/";
+    
+            const api=`${proxy}api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=02060e118da723a8af10b3e7a7f0042c    `
+    
+            fetch(api).then((response)=>{
+    
+                return response.json();
+    
+    
+            })
+            .then (data =>
+                {
+    
+                        const{name}=data;
+                        const{feels_like}=data.main;
+                        const{id,main}=data.weather[0];
+    
+    
+                        loc.textContent=name;
+                        climate.textContent=main;
+                        tempvalue.textContent=Math.round(feels_like-273);
+                        console.log(data);
+
+
+                    })
+        
+        
+     
